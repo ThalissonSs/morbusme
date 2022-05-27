50757e9da56f65a0ef7e0f40dc38810e0abdd4ba
ThalissonSs <104725713+ThalissonSs@users.noreply.github.com>
2022-05-27 08:12:40 -0300

    commit delete
    Alterações

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const Hambuguer = document.getElementById("hambuguer")
-
-function toggleMenu() {
-  const nav = document.getElementById("header-nav")
-  nav.classList.toggle('active')
-}
-
-Hambuguer.addEventListener('click', toggleMenu)
